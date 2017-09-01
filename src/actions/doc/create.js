9ed8fa8b2d074927cc8f018e6fdd60645a9747a7
alixez <alixe.z@foxmail.com>
2017-09-02 00:21:56 +0800

    fix doc create

--- src/actions/doc/create.js
+++ src/actions/doc/create.js
@@ -8,7 +8,7 @@ import DocService from '../../service/DocService';
 import {filterParams} from '../../utils/filters'
 const docService = new DocService();
 
-export default async (req, params, {response}) => {
+export default async (req, params, {response, services}) => {
   const args = filterParams(req.body, {
     doc_title: ['string', 'required'],
     doc_type: ['integer', 'required'],
@@ -25,7 +25,6 @@ export default async (req, params, {response}) => {
   const allUnreadReceivers = await services.doc.getUnreadReceivers(createdDoc.id);
 
   const unreadReceiverIDs = allUnreadReceivers.map(value => value.receiver_id);
-
 
   await services.notification.sendToPeople({
     title: `【公文】${createdDoc.doc_title}`,
