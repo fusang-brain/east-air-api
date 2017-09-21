fc457eb0f5d4780858e37a315c8d0f67ebcf8002
alixez <alixe.z@foxmail.com>
2017-09-21 15:37:50 +0800

    fix auth bug

--- src/actions/auth/login.js
+++ src/actions/auth/login.js
@@ -53,13 +53,17 @@ export default async function (req, params, {device, redisClient}) {
   const accessToken = Auth.getToken(user.id);
 
   let cacheToken = await redisClient.getAsync(`ACCESS_TOKEN_${user.id}`);
+  console.log(cacheToken);
   if (!cacheToken) {
     cacheToken = [];
     cacheToken.push(accessToken);
-    redisClient.set(`ACCESS_TOKEN_${user.id}`, cacheToken, 'EX', 7 * 24 * 60 * 60)
+    redisClient.set(`ACCESS_TOKEN_${user.id}`, cacheToken, 'EX', 7 * 24 * 60 * 60);
   } else {
+    if (typeof cacheToken === 'string') {
+      cacheToken = [];
+    }
     cacheToken.push(accessToken);
-    redisClient.set(`ACCESS_TOKEN_${user.id}`, cacheToken, 'EX', 7 * 24 * 60 * 60)
+    redisClient.set(`ACCESS_TOKEN_${user.id}`, cacheToken, 'EX', 7 * 24 * 60 * 60);
   }
 
   user.password = undefined;
