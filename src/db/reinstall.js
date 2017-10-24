5f0bca75aef9294012bdbed6539989c1c915df12
alixez <alixe.z@foxmail.com>
2017-10-24 18:42:05 +0800

    fix reinstall

--- src/db/reinstall.js
+++ src/db/reinstall.js
@@ -4,7 +4,7 @@
  * Date: 2017/10/23
  */
 
-import {iLog} from 'helper';
+import {iLog} from './helper';
 import models from '../models';
 import Auth from '../utils/auth';
 import Config from '../config';
