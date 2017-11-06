b72327df2fd3e3be604d48218b43370eeda872b0
alixez <alixe.z@foxmail.com>
2017-11-06 09:38:12 +0800

    fix user import script

--- src/db/import_user.js
+++ src/db/import_user.js
@@ -36,38 +36,49 @@ async function start () {
     if (i === 0 ) {
       continue;
     }
+    try {
+      let user = await models.User.create({
+        name: data[0],
+        mobile: data[1],
+        gender: data[2],
+        birthday: new Date(data[3]).getTime(),
+        password: Auth.encodePassword('05B530AD0FB56286FE051D5F8BE5B8453F1CD93F'), // 88888888
+        card_num: data[4],
+        ehr: data[5],
+        state: data[6],
+        no: data[7],
+        type: data[8],
+        dept: deptMapper[data[9]] ? deptMapper[data[9]].id : '',
+        role: roleMapper[data[10]] ? roleMapper[data[10]].id : '',
+        qq: data[11],
+        wechat: data[12],
+        degree: data[13],
+        duties: data[14],
+        jobs: data[15],
+        exist_job_level: data[16],
+        now_job_level: data[17],
+        start_work_time: new Date(data[18]).getTime(),
+        join_time: new Date(data[19]).getTime(),
+        integration: data[20],
+        mark: data[21],
+      });
 
-    let user = await models.User.create({
-      name: data[0],
-      mobile: data[1],
-      birthday: moment(data[2]).valueOf(),
-      password: Auth.encodePassword('05B530AD0FB56286FE051D5F8BE5B8453F1CD93F'), // 88888888
-      card_num: data[3],
-      ehr: data[4],
-      state: data[5],
-      no: data[6],
-      type: data[7],
-      dept: deptMapper[data[8]] ? deptMapper[data[8]].id : '',
-      role: roleMapper[data[9]] ? deptMapper[data[9]].id : '',
-      qq: data[10],
-      wechat: data[11],
-      degree: data[12],
-      duties: data[13],
-      jobs: data[14],
-      exist_job_level: data[15],
-      now_job_level: data[16],
-      start_work_time: moment(data[17]).valueOf(),
-      join_time: moment(data[18]).valueOf(),
-      integration: data[19],
-      mark: data[20],
+      if (deptMapper[data[9]] && user) {
+        await models.DataAccess.create({
+          user_id: user.id,
+          dept_id: deptMapper[data[9]].id,
+        });
+      }
 
-    });
+      if (user) {
+        iLog(`  -- 新增用户 ${data[0]} 成功`);
+        total += 1;
+      }
 
-    if (user) {
-      iLog(`  -- 新增用户 ${data[0]} 成功`);
-      total += 1;
+    } catch (err) {
+      // console.log(err);
+      iLog(`  -- 导入用户 ${data[0]} 出现异常 @ ${err.errors[0].message}`);
     }
-
   }
   // console.log(workSheetsFromFile);
   iLog(`* 用户导入成功 共遍历 ${sheetData.length - 1} 条数据，成功处理 ${total} 条数据`);
