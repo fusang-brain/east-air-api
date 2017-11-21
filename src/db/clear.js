28ed181aa3a9b832f240f0c30d3abf0ccafc6ecd
alixez <alixe.z@foxmail.com>
2017-11-21 13:35:03 +0800

    add db clear script

---
+++ src/db/clear.js
@@ -0,0 +1,41 @@
+/**
+ * By Alpha
+ * Author: alixez <alixe.z@foxmail.com>
+ * Date: 2017/11/20
+ */
+
+import {iLog} from './helper';
+import models from '../models';
+
+const excludeArgs = [
+  'sequelize',
+  'Sequelize',
+  'User',
+  'Role',
+  'RolePermission',
+  'Permission',
+  'Dept',
+  'DataAccess',
+]
+
+async function start () {
+
+  for (let i in models) {
+    if (excludeArgs.includes(i)) continue;
+    iLog(`清空业务表 - ${i}`);
+    await models[i].destroy({
+      where: {},
+    });
+  }
+
+  iLog('^ 处理完成 ');
+}
+
+start().then(
+  res => {
+    process.exit();
+  }
+).catch(err => {
+  console.log(err);
+  process.exit(1);
+})
