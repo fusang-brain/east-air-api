b4338621c9520831339fca7cbd31238c0a950c4f
alixez <alixe.z@foxmail.com>
2018-05-08 10:10:29 +0800

    fix video callback

--- src/actions/vod/video_callback.js
+++ src/actions/vod/video_callback.js
@@ -4,7 +4,7 @@ import {filterParams} from '../../utils/filters';
 export default async function (req, params, ctx) {
   const { services, response } = ctx;
   const { VideoId, EventType, Status, ...others } = req.body;
-  // console.log("视频回调...", req.body);
+  console.log("视频回调...", req.body);
   if (Status === "fail") {
     return {
       code: response.getSuccessCode('update'),
