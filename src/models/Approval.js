fb07840b9f77ce7f74d03859765c062d5163250f
alixez <alixe.z@foxmail.com>
2018-05-10 09:50:03 +0800

    [fix] 修改Approval模型的类型bug

--- src/models/Approval.js
+++ src/models/Approval.js
@@ -10,8 +10,8 @@ export default function (sequelize, DataTypes) {
     approval_no: {type: DataTypes.STRING, defaultValue: generateNo},
     approval_type: {type: DataTypes.INTEGER},       // 审批类型 1:活动 2:慰问 3:经费
     project_subject: {type: DataTypes.STRING},      // 项目主题
-    project_purpose: {type: DataTypes.STRING},      // 项目目的
-    project_content: {type: DataTypes.STRING},      // 项目详情
+    project_purpose: {type: DataTypes.TEXT},      // 项目目的
+    project_content: {type: DataTypes.TEXT},      // 项目详情
     project_type: {type: DataTypes.INTEGER},        // 项目类型 0:'职工教育', 1:'文体活动', 2:'宣传活动', 3:'其他活动', 4:'送温暖' , 5:'培训', 6:'会议', 7:'专项会议', 8:'其他业务', 9:'慰问审批', 10:'经费审批'
     dept_id: {type: DataTypes.UUID},                // 项目发起部门
     total_amount: {type: DataTypes.DECIMAL(10, 2)}, // 总金额
